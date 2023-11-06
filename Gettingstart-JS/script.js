9a5e75eef649f1f3ef98435c2c24edf66eb31688
LaaNoo <lano137001@gmail.com>
2023-11-06 21:13:19 +0700

    la no

---
+++ Gettingstart-JS/script.js
@@ -0,0 +1,20 @@
+let a = 6; //gán giá trị 6 cho a 
+let b = 10;
+
+let c= a +b;
+
+console.log(" Số nhập vào là " +a );
+console.log(" Số nhập vào là " +b );
+console.log("Tổng hai số nhập vào là " +c );/* ghi chú nhiều dòng*/
+
+// Hàm alert(): Hàm thông báo ra màn hình trên cùng. 
+// alert (" Tổng là: " +c);
+
+// Hàm prompt: Hiển thị thông báo nhập dữ liệu
+// prompt("Nhập vào số");
+
+// Hàm confirm(): Hiển thị thông báo xác nhận một vấn đề 
+// confirm("Bạn có muốn nhập con số này không?");
+
+document.write("Tổng là :  " +c);
+
